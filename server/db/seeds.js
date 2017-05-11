39f6a440788d677a37386eaa22424b9ad613ccc4
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-11 12:40:25 -0300

    seed with a script

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,11 +1,16 @@
+const mongoose = require('.');
 const data = require('./data.json');
 const County = require('../models/county');
 
-function seed() {
-  data.forEach(countyJSON => {
+console.log('Seeding data...');
+
+County.remove({}).then(() => {
+  Promise.all(data.map(countyJSON => {
     let county = new County(countyJSON);
     return county.save();
+  })).then(() => {
+    console.log('Finished seeding data. Closing connection...');
+    mongoose.connection.close();
+    process.exit();
   });
-}
-
-module.exports = seed;
+});
