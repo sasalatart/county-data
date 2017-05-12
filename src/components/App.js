356cf4f8626c004db99394e80406439824739cd4
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-12 12:03:13 -0300

    remove remaining unused create-react-app files

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>County Data App</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './components/App';
+import '../node_modules/bootswatch/paper/bootstrap.min.css';
 import './styles/index.css';
 
 ReactDOM.render(
