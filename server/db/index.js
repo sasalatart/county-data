76d8c7d6a403ec8dd260de5aecdd2dd88222f2c1
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-17 10:37:17 -0400

    add new env variable for full mongo uri

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const bluebird = require('bluebird');
 
+const MONGODB_URI = process.env.MONGODB_URI;
 const DB_HOST = process.env.MONGODB_HOST || 'mongo_db';
 const DB_PORT = process.env.MONGODB_PORT || '27017';
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/county-data`);
+mongoose.connect(MONGODB_URI || `mongodb://${DB_HOST}:${DB_PORT}/county-data`);
 mongoose.Promise = bluebird;
 
 module.exports = mongoose;
