d58fc38ca3249a28095df00946cbd09bdad80d64
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-05-17 09:32:38 -0400

    change db name

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const bluebird = require('bluebird');
 
 const DB_HOST = process.env.MONGODB_HOST || 'mongo_db';
 const DB_PORT = process.env.MONGODB_PORT || '27017';
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/acmKiosk`);
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/county-data`);
 mongoose.Promise = bluebird;
 
 module.exports = mongoose;
